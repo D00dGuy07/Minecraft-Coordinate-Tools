28c1e3461e0721238091385b7cbe7923be8369d6
D00dGuy07 <60475635+D00dGuy07@users.noreply.github.com>
2020-06-25 13:13:59 -0700

    Version 1.0

---
+++ Errors.py
@@ -0,0 +1,8 @@
+class Vector3ValueError(ValueError):
+    pass
+
+class Vector2ValueError(ValueError):
+    pass
+
+class ParsingError(Exception):
+    pass
---
+++ LocationList.py
@@ -0,0 +1,227 @@
+# List Location creates a list of locations tied to names
+# By Kieran Brown
+
+name = "Locations List"
+description = "Creates a set of coordinates with names"
+
+
+def getInfo():
+    return "{0} - {1}".format(name, description)
+
+commands = {
+    "add" : "[Name] [X] [Y] [Z] | Adds location to the list",
+    "list" : "| Shows list",
+    "remove" : "[Name] | Removes entry",
+    "rename" : "[Name] [NewName] | Renames entry",
+    "edit" : "[Name] [New X] [New Y] [New Z] | Edits entry",
+    "exit" : "| Closes application",
+    "save" : "[Name] | Saves list",
+    "open" : "| Opens prompt to open a list"
+}
+
+locationsList = {}
+saved = True
+
+def start(library):  
+    print('Locations List Tool\n\nJust type "exit" to go back to the main menu.\nFor names if you want spaces then surround it with "(curly brackets)"\n')
+    
+    print("List of commands:")
+
+    defaultSpace = 35
+    for command, information in commands.items():
+        barPosition = information.find("|")
+        spaces = defaultSpace - barPosition - len(command)
+        parameters = " " + information[:barPosition]
+        description = information[barPosition:]
+        
+
+        print("{0}{1}{3}{2}".format(command, parameters, description, " " * spaces))
+    print("")
+
+
+    while True:
+        arguments = seperateArguments(input())
+        if len(arguments) < 0:
+            print("You have to use a command.")
+        elif arguments[0] in commandAssignedFunctions.keys():
+            result = commandAssignedFunctions[arguments[0]](arguments, library)
+            if result == "exit":
+                break
+
+
+
+def seperateArguments(command):
+    arguments = []
+    while True:
+        if command == "":
+            break
+        
+        if command.startswith("("):
+            endBracket = command.find(")")
+            arguments.append(command[1:endBracket])
+            command = command[endBracket + 2:]
+        else:
+            firstSpace = command.find(" ")
+            if firstSpace == -1:
+                arguments.append(command)
+                break
+            
+            arguments.append(command[:firstSpace])
+            command = command[firstSpace + 1:]
+    
+    return arguments
+
+#Command Functions
+
+def add(arguments, library):
+    global saved
+    if len(arguments) != 5:
+        print("You did not inclue the exact amount of info required for the add command.")
+        return
+    elif arguments[1] in locationsList.keys():
+        print("There is already an entry in this list under this name. Would you like to overwrite it? (y/n)")
+        decision = input()
+        if decision == "y":
+            edit(arguments, library)
+        else:
+            return
+    elif not(library.RepresentsInt(arguments[2]) and library.RepresentsInt(arguments[3]) and library.RepresentsInt(arguments[4])):
+        print("You must put numbers as the coordinates.")
+        return
+    else:
+        newLocation = library.Vector3(int(arguments[2]), int(arguments[3]), int(arguments[4]))
+        locationsList[arguments[1]] = newLocation
+        saved = False
+
+    
+
+def _list(arguments, library):
+    if len(arguments) > 1:
+        print("The list command does not require parameters.")
+        return
+    counter = 0
+    for name, location in locationsList.items():
+        counter += 1
+        print("{0}) {1} | {2}".format(counter, name, str(location)))
+    
+
+
+def remove(arguments, library):
+    global saved
+    if len(arguments) != 2:
+        print("You did not inclue the exact amount of info required for the remove command.")
+        return
+    elif not(arguments[1] in locationsList.keys()):
+        print("There is no entry in this list under the name {0}".format(arguments[1]))
+        return
+    else:
+        locationsList.pop(arguments[1])
+        saved = False
+
+def rename(arguments, library):
+    global saved
+    if len(arguments) != 3:
+        print("You did not inclue the exact amount of info required for the rename command.")
+        return
+    elif not(arguments[1] in locationsList.keys()):
+        print("There is no entry in this list under the name {0}".format(arguments[1]))
+    else:
+        locationsList[arguments[2]] = locationsList.pop(arguments[1])
+        saved = False
+
+def edit(arguments, library):
+    global saved
+    if len(arguments) != 5:
+        print("You did not inclue the exact amount of info required for the edit command.")
+        return
+    elif not(arguments[1] in locationsList.keys()):
+        print("There is no entry under that name. Would you like to make one?(y/n)")
+        decision = input()
+        if decision == "y":
+            add(arguments, library)
+        else:
+            return
+    elif not(library.RepresentsInt(arguments[2]) and library.RepresentsInt(arguments[3]) and library.RepresentsInt(arguments[4])):
+        print("You must put numbers as the coordinates.")
+        return
+    else:
+        newLocation = library.Vector3(int(arguments[2]), int(arguments[3]), int(arguments[4]))
+        locationsList[arguments[1]] = newLocation
+        saved = False
+
+def _exit(arguments, library):
+    if saved:
+        return "exit"
+    else:
+        print("You have not saved your list, are you sure you would like to exit?(y/n)")
+        
+        decision = input()
+        if decision == "y":
+            return "exit"
+        else:
+            return
+
+def save(arguments, library):
+    global saved
+    if len(arguments) != 2:
+        print("You did not inclue the exact amount of info required for the save command.")
+        return
+    elif len(locationsList) < 1:
+        print("You need to add something to the list before it can be saved.")
+        return
+    elif library.DataBase.CheckDictionaryFile(arguments[1]):
+        print("The file you would like to save to already exists:\n1) Overwrite the file\n2) See contents of file\n3) Cancel operation")
+        while True:
+            decision = input()
+            if not(library.RepresentsInt(decision)):
+                print("You have to put a number.")
+            elif int(decision) < 1 or int(decision) > 3:
+                print("You have to put a number in the range of 1 to 3.")
+            elif int(decision) == 1:
+                library.DataBase.SaveDictionary(arguments[1], locationsList)
+                saved = True
+                return
+            elif int(decision) == 2:
+                #Not Possible yet
+                break
+            elif int(decision) == 3:
+                return
+    else:
+        library.DataBase.SaveDictionary(arguments[1], locationsList)
+        saved = True
+ 
+def _open(arguments, library):
+    global saved
+    if len(arguments) > 1:
+        print("You don't need any parameters for the open command")
+    else:
+        files = library.DataBase.GetDictionaryFiles()
+        print("Chose a file to open:")
+        count = 0
+        for _file in files:
+            count += 1
+            print("{0}) {1}".format(count, _file[:-5]))
+        print("")
+        fileToOpen = input()
+        loadedFile = None
+        while True:
+            if library.RepresentsInt(fileToOpen):
+                global locationsList
+                locationsList = library.DataBase.LoadDictionary(files[int(fileToOpen) - 1][:-5])
+                saved = True
+                break
+            else:
+                print("Pick the number of the file.")
+                fileToOpen = input()
+    
+
+commandAssignedFunctions = {
+    "add" : add,
+    "list" : _list,
+    "remove" : remove,
+    "rename" : rename,
+    "edit" : edit,
+    "exit" : _exit,
+    "save" : save,
+    "open" : _open
+}
